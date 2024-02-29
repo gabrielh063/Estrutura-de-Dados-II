da1c5e7e4eef9aa5144d779f1e84fde76df192af
Gabriel Henrique Alves Cintra <126260583+gabrielh063@users.noreply.github.com>
2024-02-28 22:30:05 -0300

    Atualizar o app.py

--- Estrutura de Dados II/projeto_flask/src/app.py
+++ Estrutura de Dados II/projeto_flask/src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+
 
 #  instaciar a aplicacao a um objeto
 app = Flask(__name__)
@@ -7,6 +8,13 @@ app = Flask(__name__)
 
 def index():
     return "<h1 align='center'>funcionante</h1>"
+
+@app.route("/calcula", methods=["GET"])
+# http://127.0.0.1:5000/calcula?qtd=20&preco=10
+def calcula():
+    qtd = int(request.args.get("qtd"))
+    preco = int(request.args.get('preco'))
+    return f"<h1 align='center'>R${qtd*preco}</h1>"
 
 # iniciar o app
 # usar somente em area de desenvolvimento, nao de producao
