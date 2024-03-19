bc40716c8fca196f33a6308378ed5d1e72053dbe
gabrielh063 <gabrielhalves006@gmail.com>
2024-03-18 21:20:25 -0300

    .

--- Estrutura de Dados II/projeto_flask/src/busca_ibge.py
+++ Estrutura de Dados II/projeto_flask/src/busca_ibge.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from ibge import busca 
+from ibge import busca, soma_frequencia, valor_min, valor_max, ordem_crescente
 
 app = Flask(__name__)
 
@@ -9,9 +9,48 @@ def busca_nome():
         var_nome = request.args.get("nome")
         response = busca(var_nome)
         # percorrer o retorno do busca e somar todas as ocorrencias do nome
-        return response
+        soma = soma_frequencia(response)
+        objeto_retorno = {
+            "nome_procurado": var_nome,
+            "total_ocorrencias": soma
+        }
+        return objeto_retorno
     except Exception as e:
         return f"Falha na rota /busca_nome: {e}"
 
+@app.route("/frequencia_min")
+def minimo():
+    try:
+        nome = request.args.get("nome")
+        periodo, minimo = valor_min(nome)
+        return {
+            "nome": nome,
+            "frequencia_minima": minimo,
+            "periodo": periodo
+        }
+    except Exception as e:
+        return f"falha na rota /frequencia_min: {e}"
+
+@app.route("/frequencia_max")
+def maximo():
+    try:
+        nome = request.args.get("nome")
+        periodo, minimo = valor_max(nome)
+        return {
+            "nome": nome,
+            "frequencia_maxima": minimo,
+            "periodo": periodo
+        }
+    except Exception as e:
+        return f"falha na rota {e}"
+    
+@app.route("/ordem_crescente")
+def crescente():
+    try:
+        nome = request.args.get("nome")
+        lista = ordem_crescente(nome)
+        return lista
+    except Exception as e:
+        return f'falha na rota /ordem_crescente:  {e}'
 
 app.run(debug=True)
--- Estrutura de Dados II/projeto_flask/src/ibge.py
+++ Estrutura de Dados II/projeto_flask/src/ibge.py
@@ -5,5 +5,53 @@ def busca(nome):
     resposta = requests.get(url)
     return resposta.json()
 
-retorno = busca("jose")
-print(retorno)
+def get_res(obj):
+    if len(obj)>0:
+        conteudo = obj[0]
+        return conteudo.get("res", [])
+    else:
+        return []
+
+def soma_frequencia(var_json):
+    conteudo = var_json[0]
+    resposta = conteudo.get("res", [])
+    soma = []
+    for elemento in resposta:
+        frequencia = elemento.get("frequencia", 0)
+        soma.append(frequencia)        
+    return sum(soma) 
+
+def valor_min(nome):
+    dados = busca(nome)
+    lista = get_res(dados)
+    if len(lista)>0:
+        minimo = lista[0]["frequencia"]
+        periodo = lista[0]["periodo"]
+    for index in range(1, len(lista)):
+        if lista[index]["frequencia"] < minimo:
+            minimo = lista[index]["frequencia"]
+            periodo = lista[index]["periodo"]
+    return periodo, minimo
+
+def valor_max(nome):
+    dados = busca(nome)
+    lista = get_res(dados)
+    if len(lista)>0:
+        maximo = lista[0]["frequencia"]
+        periodo = lista[0]["periodo"]
+    for index in range(1, len(lista)):
+        if lista[index]["frequencia"] > maximo:
+            maximo = lista[index]["frequencia"]
+            periodo = lista[index]["periodo"]
+    return periodo, maximo
+
+def ordem_crescente(nome):
+    dados = busca(nome)
+    lista = get_res(dados)
+    for i in range(0, len(lista) -1):
+        menor = i
+        for j in range(i+1, len(lista)):
+            if lista[menor]["frequencia"] > lista[j]["frequencia"]:
+                menor = j
+            lista[i], lista[menor] = lista[menor], lista[i]
+    return lista
