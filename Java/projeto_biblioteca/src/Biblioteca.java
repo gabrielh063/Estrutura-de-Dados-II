603ed5f3d70d19638d1b5636d1cbcaa43fd8327d
gabrielh063 <gabrielhalves006@gmail.com>
2024-06-03 21:42:25 -0300

    comeco de trabalho

---
+++ Java/projeto_biblioteca/src/Biblioteca.java
@@ -0,0 +1,45 @@
+
+import java.util.LinkedList;
+import javax.swing.event.ListDataEvent;
+
+public class Biblioteca {
+    private LinkedList<Livro> dados;
+
+    public Biblioteca(){
+        dados = new LinkedList<Livro>();
+    }
+    public void inserir(Livro livro){
+        if(!dados.contains(livro)){
+            dados.add(livro);
+        } else{
+            System.out.println(livro + " O livro ja existe na biblioteca");
+        }
+        System.out.println("Inserir");
+        System.out.println(dados);
+    }
+    public void mostrarTodos(){
+        for (Livro livro : dados) {
+            System.out.println(livro);
+        }
+    }
+    public void buscarId(int idProcurado){
+        Livro idEncontrado = null;
+        for (Livro idLivro : dados) {
+            if (idLivro.getId() == idProcurado) {
+                idEncontrado = idLivro;
+                System.out.println(idEncontrado + "Esta aqui");
+                break; 
+            } 
+        }
+        System.out.println("Livro nao encontrado"); 
+    }
+    public void removeLivros(int idLivro){
+        for (Livro livro : dados) {
+            if (livro.getId() == idLivro)
+            dados.remove(livro);
+            System.out.println(livro + " removido com sucesso");
+            break;
+        }
+        System.out.println("Livro nao encontrado");
+    }
+}
